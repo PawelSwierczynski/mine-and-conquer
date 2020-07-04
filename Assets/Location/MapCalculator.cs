a100467398afd0c6d0cac6ae8895effe0c01ccaa
Paweł Świerczyński <pawelswierczynski@pro.wp.pl>
2020-07-04 23:21:31 +0200

    Adjust zoom calculation for different latitudes

---
+++ Assets/Location/MapCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assets.Location
+{
+    public class MapCalculator
+    {
+        private const double MAP_WIDTH_IN_METERS = 165.443;
+        private const double MAP_HEIGHT_IN_METERS = 294.121;
+        private const int MAP_WIDTH_IN_PIXELS = 450;
+        private const int MAP_HEIGHT_IN_PIXELS = 800;
+
+        private double CalculateCosinus(double value)
+        {
+            return Math.Cos(value * Math.PI / 180);
+        }
+
+        public string RetrieveMapResolution()
+        {
+            return MAP_WIDTH_IN_PIXELS.ToString() + "x" + MAP_HEIGHT_IN_PIXELS.ToString();
+        }
+
+        public double CalculateMapZoom(float latitude)
+        {
+            return Math.Log10(212896.0f * CalculateCosinus(latitude)) / 0.30103f;
+        }
+
+        //1 degree of latitude is equal to 111.32 km
+        //1 degree of longitude is equal to about 111.319491 * cos(latitude) km
+    }
+}
--- Assets/Location/MapboxTextureProvider.cs
+++ Assets/Location/MapboxTextureProvider.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
-using System.IO;
-using System.Net;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -10,10 +9,20 @@ namespace Assets.Location
     public class MapboxTextureProvider : MonoBehaviour
     {
         private const string API_URL_BEGINNING = "https://api.mapbox.com/styles/v1/kagalorn/ckc2bflbx6sju1is2s6m3t768/static/";
-        private const string API_URL_END = ",17,0/450x800?access_token=pk.eyJ1Ijoia2FnYWxvcm4iLCJhIjoiY2tjMmIyMmlkMXR3MDJybGpqNmZkZTFieSJ9.kDc986jc3zuBGs2iclzcPA";
+        private const string API_URL_ROTATION = ",0/";
+        private const string API_URL_END = "?access_token=pk.eyJ1Ijoia2FnYWxvcm4iLCJhIjoiY2tjMmIyMmlkMXR3MDJybGpqNmZkZTFieSJ9.kDc986jc3zuBGs2iclzcPA";
 
         public RawImage gpsMapRawImage;
         public GPS gps;
+
+        private MapCalculator mapCalculator;
+        private CultureInfo apiCulture;
+
+        void Start()
+        {
+            mapCalculator = new MapCalculator();
+            apiCulture = CultureInfo.CreateSpecificCulture("en-US");
+        }
 
         void Update()
         {
@@ -40,9 +49,7 @@ namespace Assets.Location
 
         private string GenerateAPIRequestAddress(float longitude, float latitude)
         {
-            Debug.Log(API_URL_BEGINNING + longitude.ToString() + "," + latitude.ToString() + API_URL_END);
-
-            return API_URL_BEGINNING + longitude.ToString() + "," + latitude.ToString() + API_URL_END;
+            return API_URL_BEGINNING + longitude.ToString(apiCulture) + "," + latitude.ToString(apiCulture) + "," + mapCalculator.CalculateMapZoom(latitude).ToString(apiCulture) + API_URL_ROTATION + mapCalculator.RetrieveMapResolution() + API_URL_END;
         }
     }
 }
