af1764d930a1cd828c6e28f988f5c73e88144354
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2020-07-27 14:05:51 +0200

    login

---
+++ Assets/Login.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+    public GameObject username;
+    public GameObject passowrd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
