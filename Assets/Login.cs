9e5c147306971f1c90a493d9f506fa1c7f13cd01
pawelswierczynski <pawelswierczynski@pro.wp.pl>
2020-09-14 18:31:19 +0200

    Fix login post request

--- Assets/Login.cs
+++ Assets/Login.cs
@@ -1,37 +1,27 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 
 public class Login : MonoBehaviour
 {
-    public GameObject UsernameInput;
-    public GameObject PasswordInput;
-    public GameObject LoginButton;
-    private string Username;
-    private string Password;
-    // Start is called before the first frame update
+    public InputField UsernameInput;
+    public InputField PasswordInput;
+    public Button LoginButton;
+
     void Start()
     {
-        UnityEngine.UI.Button button = LoginButton.GetComponent<UnityEngine.UI.Button>();
-        button.onClick.AddListener(LoginRequest);
+        LoginButton.onClick.AddListener(LoginRequest);
     }
-    // Update is called once per frame
-    void Update()
-    {
-        Username = UsernameInput.GetComponent<UnityEngine.UI.InputField>().text;
-        Password = PasswordInput.GetComponent<UnityEngine.UI.InputField>().text;
-    }
+
     IEnumerator LoginRequestSend()
     {
         WWWForm form = new WWWForm();
-        form.AddField(Username, Password);
-        
-        string requeststring = "{\"Login\":" + "\"" + Username + "\"" + ",\"Password\":" + "\"" + Password + "\"}";
-        Debug.Log(requeststring);
-        using (UnityWebRequest www = UnityWebRequest.Post("https://rest-api-nodejs-mysql-server.herokuapp.com/Users/Login", requeststring))
+        form.AddField("Login", UsernameInput.text);
+        form.AddField("Password", PasswordInput.text);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://rest-api-nodejs-mysql-server.herokuapp.com/Users/Login", form))
         {
-            www.SetRequestHeader("Content-Type", "application/json");
             yield return www.SendWebRequest();
 
             if (www.isNetworkError || www.isHttpError)
