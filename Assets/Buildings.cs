393798fc88ed3ae44fd0186f9c96d3e9be5cf5fb
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2020-09-22 10:39:24 +0200

    village

---
+++ Assets/Buildings.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Buildings : MonoBehaviour
+{
+    public Button LumberjackButton;
+    public Button StonequarryButton;
+    public Button GoldmineButton;
+
+    void Start()
+    {
+        LumberjackButton.onClick.AddListener(Lumberjack);
+        StonequarryButton.onClick.AddListener(Stonequarry);
+        GoldmineButton.onClick.AddListener(Goldmine);
+    }
+
+    void Update()
+    {
+        
+    }
+    public void Lumberjack()
+    {
+        SceneManager.LoadScene("Lumberjack");
+    }
+    public void Stonequarry()
+    {
+        //SceneManager.LoadScene("Stonequarry");
+    }
+    public void Goldmine()
+    {
+        //SceneManager.LoadScene("Goldmine");
+    }
+}
---
+++ Assets/Lumberjack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Lumberjack : MonoBehaviour
+{
+    public Button UpgradeButton;
+    public Button BackButton;
+    public Text CurrentLevel;
+    public Text CurrentProduction;
+    public Text NextLevel;
+    public Text NextProduction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpgradeButton.onClick.AddListener(UpgradeRequest);
+        BackButton.onClick.AddListener(BackRequest);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpgradeRequest()
+    {
+        CurrentLevel.text = NextLevel.text;
+        CurrentProduction.text = NextProduction.text;
+        NextLevel.text = (int.Parse(NextLevel.text) + 1).ToString();
+        NextProduction.text = "+" + (int.Parse(NextProduction.text) + 2).ToString();
+    }
+    public void BackRequest()
+    {
+        SceneManager.LoadScene("Village");
+    }
+}
